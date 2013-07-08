d9118c40e5d4c92969353b61b7a910492cbe6928
critium <fdelacruz@gmail.com>
2013-07-08 16:59:21 -0400

    init commit

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "nameserver 208.67.222.222" | sudo tee /etc/resolvconf/resolv.conf.d/base > /dev/null
+sudo apt-get update
+
+# sudo cp /vagrant/vagrantjdk.bin /opt
+# sudo chmod +x /opt/vagrantjdk.bin
+# cd /opt
+# sudo ./vagrantjdk.bin
+# sudo mkdir -p /usr/lib/jvm/jdk1.6
+# sudo mv jdk1.6*/* /usr/lib/jvm/jdk1.6
+# sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.6/bin/java" 1
+# sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.6/bin/javaws" 1
+
+#sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password g0lfsw1ng'
+#sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password g0lfsw1ng'
+# sudo apt-get -y install mysql-server
+##DONT NEED## mysqladmin -u root password g0lfsw1ng
+# mysql -uroot -pg0lfsw1ng -e "create database betareturn;";
+# mysql -uroot -pg0lfsw1ng -e "create database betareturn_scratch;";
+
+# sudo apt-get -y install git
+
+# mkdir -p /home/vagrant/code
+# ln -fs /vagrant/code /home/vagrant/code
+
+
+
