95d0449b5b828793f5cb0eed404258a3627f0aa7
critium <fdelacruz@gmail.com>
2013-08-22 10:52:58 -0400

    almost ready

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,21 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "precise32"
-  config.vm.provision :shell, :path => "bootstrap.sh"
-  config.vm.network :forwarded_port, host: 19000, guest: 9000
+	config.vm.provision :shell, :inline => "echo Hello"
+	config.vm.box = "precise32"
+
+	config.vm.define :web1 do |web1|
+		config.vm.provision :shell, :path => "bootstrap.sh"
+		config.vm.network :forwarded_port, host: 19000, guest: 9000
+		config.vm.network :private_network, ip: "10.0.0.1"
+		#config.vm.synced_folder ".", "/vagrant", :nfs => true
+	end
+
+	config.vm.define :web2 do |web2|
+		config.vm.provision :shell, :path => "bootstrap.sh"
+		config.vm.network :forwarded_port, host: 19001, guest: 9000
+		config.vm.network :private_network, ip: "10.0.0.2"
+		#config.vm.synced_folder ".", "/vagrant", :nfs => true
+	end
+
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,25 +3,26 @@
 echo "nameserver 208.67.222.222" | sudo tee /etc/resolvconf/resolv.conf.d/base > /dev/null
 sudo apt-get update
 
-# cd /vagrant
-# sudo cat vagrantjdk.bin.parts?? > vagrantjdk.bin
-# sudo cp /vagrant/vagrantjdk.bin /opt
-# sudo chmod +x /opt/vagrantjdk.bin
-# cd /opt
-# sudo ./vagrantjdk.bin
-# sudo mkdir -p /usr/lib/jvm/jdk1.6
-# sudo mv jdk1.6*/* /usr/lib/jvm/jdk1.6
-# sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.6/bin/java" 1
-# sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.6/bin/javaws" 1
+cd /vagrant
+sudo cat vagrantjdk.bin.part?? > vagrantjdk.bin
+sudo cp /vagrant/vagrantjdk.bin /opt
+sudo chmod +x /opt/vagrantjdk.bin
+cd /opt
+sudo ./vagrantjdk.bin
+sudo mkdir -p /usr/lib/jvm/jdk1.6
+sudo mv jdk1.6*/* /usr/lib/jvm/jdk1.6
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.6/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.6/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.6/bin/javac" 1
 
-#sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password g0lfsw1ng'
-#sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password g0lfsw1ng'
-# sudo apt-get -y install mysql-server
-##DONT NEED## mysqladmin -u root password g0lfsw1ng
-# mysql -uroot -pg0lfsw1ng -e "create database betareturn;";
-# mysql -uroot -pg0lfsw1ng -e "create database betareturn_scratch;";
+sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password g0lfsw1ng'
+sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password g0lfsw1ng'
+sudo apt-get -y install mysql-server
+#DONT NEED## mysqladmin -u root password g0lfsw1ng
+mysql -uroot -pg0lfsw1ng -e "create database betareturn;";
+mysql -uroot -pg0lfsw1ng -e "create database betareturn_scratch;";
 
-# sudo apt-get -y install git
+sudo apt-get -y install git
 
 # mkdir -p /home/vagrant/code
 # ln -fs /vagrant/code /home/vagrant/code
